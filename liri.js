407bb6f592a4fdc8a71224cfe79810cab313996c
liannaccone <lciannaccone@gmail.com>
2018-02-13 21:48:43 -0800

    adding do-what-it-says function, updating random.txt to omdb params for now to test

--- liri.js
+++ liri.js
@@ -4,20 +4,68 @@ require("dotenv").config();
 // assigning variable for request npm package
 var request = require("request");
 
+var twitter = require('twitter');
+
+// fs is a core Node package for reading and writing files
+var fs = require("fs");
+
 var keys = require("./keys.js");
 
   // var spotify = new Spotify(keys.spotify);
   // var client = new Twitter(keys.twitter);
 
 // variable storing the user operation
-var nodeArg = process.argv
+var nodeArgs = process.argv
 var userOper = process.argv[2]
+var userInput = process.argv[3]
 
-// if the user input movie-this as an operator then...
-if(userOper === "movie-this") {
 
-	// assign the movie name to a variable, need to make this work for names with spaces
-	var movieName = process.argv[3];
+// switch statement to run the function corresponding to the operation the user input (userOper)
+switch (userOper) {
+	case "movie-this":
+		runMovieThis(userInput);
+		break;
+
+	// case "my-tweets":
+	// 	runMyTweets();
+	// 	break;
+
+	// case "spotify-this-song":
+	// 	runSpotifyThisSong();
+	// 	break;
+
+	case "do-what-it-says":
+		runDoWhatItSays();
+		break
+}
+
+
+
+// =============================================
+//       MOVIE-THIS
+// =============================================
+
+
+function runMovieThis(userInput) {
+
+
+	var movieName = ""
+
+	// if statement checking to see if the user input a movie, if they did....
+	if (userInput){
+		for (var i = 3; i < nodeArgs.length; i++) {
+	  		if (i > 3 && i < nodeArgs.length) {
+	    		movieName = movieName + "+" + nodeArgs[i];
+	  		}
+	  		else {
+	    		movieName += nodeArgs[i];
+	 		 }
+		};
+	}
+	// if user did not put a movie in, default to Mr Nobody
+	else{
+		movieName = "mr+nobody";
+	}
 
 	// ... run a request to the OMDB API with the movie specified
 	request("http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy", function(error, response, body) {
@@ -25,16 +73,78 @@ if(userOper === "movie-this") {
  		 // If the request is successful (i.e. if the response status code is 200)
  		 if (!error && response.statusCode === 200) {
 
-    	console.log("* Title: " + JSON.parse(body).Title);
-    	console.log("* Year: " + JSON.parse(body).Year);
-    	console.log("* IMDB Rating: " + JSON.parse(body).imdbRating);
-    	console.log("* Rotton Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-    	console.log("* Country Produced: " + JSON.parse(body).Country);
-     	console.log("* Language: " + JSON.parse(body).Language);
-    	console.log("* Plot: " + JSON.parse(body).Plot);
-    	console.log("* Actors: " + JSON.parse(body).Actors);
-  }
+	 		// then print the following information...
+	    	console.log	("\n* Title:", JSON.parse(body).Title,
+	    				 "\n* Year:", JSON.parse(body).Year,
+	    				 "\n* IMDB Rating:", JSON.parse(body).imdbRating,
+	    				 "\n* Rotton Tomatoes Rating:", JSON.parse(body).Ratings[1].Value,
+	    				 "\n* Country Produced:", JSON.parse(body).Country,
+	     				 "\n* Language:", JSON.parse(body).Language,
+	    				 "\n* Plot:", JSON.parse(body).Plot,
+	    				 "\n* Actors:", JSON.parse(body).Actors, "\n")
+  		}
+	});
+} // closing runMovieThis
+
+
+// =============================================
+//       MY-TWEETS
+// =============================================
+
+// function runMyTweets() {
+
+// var client = new twitter(keys.twitter);
+
+// };
+
+
+// =============================================
+//       SPOTIFY-THIS-SONG
+// =============================================
+
+// function runSpotifyThisSong() {
+
+
+// };
+
+
+
+// =============================================
+//       DO-WHAT-IT-SAYS
+// =============================================
+
+
+function runDoWhatItSays() {
+
+	fs.readFile("random.txt", "utf8", function(error, data) {
+
+	  if (error) {
+	    return console.log(error);
+	  }
+
+		var dataArr = data.split(",");
+
+		var randomOper = dataArr[0];
+		var randomInput = dataArr[1];
+
+
+		switch (randomOper) {
+			case "movie-this":
+			runMovieThis(randomInput);
+			break;
+
+		// case "my-tweets":
+		// 	runMyTweets();
+		// 	break;
+
+		// case "spotify-this-song":
+		// 	runSpotifyThisSong();
+		// 	break;
+		}
+
 });
+};
 
 
-}
+
+
